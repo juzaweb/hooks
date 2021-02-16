63fa1af4f4490b65c7ca6427e340bfa38a437936
Đặng Thế Anh <dangtheanh16@gmail.com>
2021-02-16 12:54:05 +0700

    first commit

---
+++ src/Facades/Events.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Theanh\LaravelHooks\Facades;
+
+class Events extends Eventy
+{
+    // for backwards compatibility
+}
---
+++ src/LaravelHooksServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Theanh\LaravelHooks;
+
+use Illuminate\Support\ServiceProvider;
+use Theanh\LaravelHooks\Hooks\Events;
+
+class LaravelHooksServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    
+    }
+    
+    public function register()
+    {
+        // Registers the eventy singleton.
+        $this->app->singleton('eventy', function ($app) {
+            return new Events();
+        });
+        
+        // Register service providers
+        //$this->registerServiceProvider();
+        
+        // Register helper file
+        require_once(__DIR__ . '/Helpers/helpers.php');
+    }
+    
+    protected function registerServiceProvider() {
+    
+    }
+}
